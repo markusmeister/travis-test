5fd436fa0bb86fbdd33fdc150e7bc4333eb2fc0f
Markus Meister <meister4@mac.com>
2020-03-02 11:32:24 -0800

    retrieve after failure

---
+++ _retrieve.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+[ -z "${GITHUB_PAT}" ] && exit 0
+[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
+
+git config --global user.email "meister4@mac.com"
+git config --global user.name "Markus Meister"
+
+git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
+cd book-output
+cp -r ../bookdown-demo.Rmd ./ # retrieve the knitted file
+git add --all *
+git commit -m"Update the book" || true
+git push -q origin gh-pages
